bc62b84ab881e792277df6158c05708c9e02b5c3
bilimerdi <orunerdi186@gmail.com>
2023-08-25 08:37:16 +0300

    Update Project
    react-navigation-stack ve react-navigation kütüphaneleri projeye entegre edildi

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import MainScreen from "./src/screens/MainScreen";
+import LoginScreen from "./src/screens/LoginScreen";
+import LogUpScreen from "./src/screens/LogUpScreen";
+import ProductScreen from "./src/screens/ProductScreen";
+import MenuScreen from "./src/screens/MenuScreen";
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
 
 const navigator = createStackNavigator(
   {
     Main: MainScreen,
+    Login: LoginScreen,
+    LogUp: LogUpScreen,
+    Product: ProductScreen,
+    Menu: MenuScreen,
   },
   {
     initialRouteName: "Main",
