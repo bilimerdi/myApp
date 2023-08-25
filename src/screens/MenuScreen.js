eb8329bbcd4d6bb88199934c66f996bf90e852d5
bilimerdi <orunerdi186@gmail.com>
2023-08-25 08:40:05 +0300

    Create MenuScreen.js
    MenuScreen.js sayfası oluşturuldu.

---
+++ src/screens/MenuScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const MenuScreen = () => {
+  return (
+    <View>
+      <Text>menuscreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default MenuScreen;
